fae17730d369f20a4eeb8f8e5daf974038300a50
Sladriix <sladriix@gmail.com>
2024-06-05 11:47:58 +0200

    Update index.html to use script-native.js for the search functionality and include script.js as the base file

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const recipesContainer = document.getElementById('recipes-container');
+document.addEventListener("DOMContentLoaded", () => {
+  const recipesContainer = document.getElementById("recipes-container");
 
-    // Fonction pour afficher les recettes
-    const displayRecipes = (recipes) => {
-        recipesContainer.innerHTML = '';
-        recipes.forEach(recipe => {
-            const recipeCard = document.createElement('div');
-            recipeCard.className = 'bg-white p-4 rounded shadow';
-            recipeCard.innerHTML = `
+  // Fonction pour afficher les recettes
+  const displayRecipes = (recipes) => {
+    recipesContainer.innerHTML = "";
+    recipes.forEach((recipe) => {
+      const recipeCard = document.createElement("div");
+      recipeCard.className = "bg-white p-4 rounded shadow";
+      recipeCard.innerHTML = `
                 <img src="images/${recipe.image}" alt="${recipe.name}" class="w-full h-32 object-cover rounded mb-4">
                 <h2 class="text-xl font-bold mb-2">${recipe.name}</h2>
                 <p>${recipe.description}</p>
                 <p class="text-gray-600 mt-2">${recipe.time} min</p>
             `;
-            recipesContainer.appendChild(recipeCard);
-        });
-    };
+      recipesContainer.appendChild(recipeCard);
+    });
+  };
 
-    // Initialiser l'affichage avec toutes les recettes
-    displayRecipes(recipes);
+  // Initialiser l'affichage avec toutes les recettes
+  displayRecipes(recipes);
 });
